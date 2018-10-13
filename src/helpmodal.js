6c4f73d1929e5ac220f00f73ac20d5f376235155
Greg Morgan <34179799+vibraniumforge@users.noreply.github.com>
2018-10-13 15:22:41 -0700

    Revert "Niky edits"

--- src/helpmodal.js
+++ src/helpmodal.js
@@ -1,26 +1,43 @@
 import React from "react";
 import Modal from "react-modal";
 
-// react fragments are only necessary when you need to wrap adjacent elements
-export default ({showModal, onRequestClose}) =>  (
-  <Modal
-    className="modal"
-    appElement={document.getElementById("app")}
-    isOpen={showModal}
-    ariaHideApp={false}
-  >
-    <p>
-      To play, choose a value between 1 and 5 with the Bet One button.
-      Then press "Deal" to deal a hand. You have the opportunity to redraw
-      up to fice cards onc time only. Winners are paid according to the
-      pay table. If you get less than a pair of Jacks, you lose.
-    </p>
-    <button
-      type="button"
-      id="close-modal-button "
-      onClick={onRequestClose}
-    >
-      Close Modal
-    </button>
-  </Modal>
-);
+class HelpModal extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Modal
+          className="modal"
+          appElement={document.getElementById("app")}
+          isOpen={this.props.showHelpModal}
+          ariaHideApp={false}
+        >
+          <p>
+            To play, choose a value between 1 and 5 with the Bet One button. The
+            default is 1.
+          </p>
+          <p>Then press "Deal" to deal a hand.</p>{" "}
+          <p>
+            You have the opportunity to redraw up to five cards one time only.{" "}
+          </p>
+          <p>
+            You will receive that many new cards to make a final five card poker
+            hand.
+          </p>
+          <p>The hand is completed.</p>
+          <p>
+            Winners are paid according to the pay table and amount wagered.
+          </p>{" "}
+          <p>If you get less than a pair of Jacks, you lose.</p>
+          <button
+            type="button"
+            className="close-modal-button "
+            onClick={this.props.onRequestClose}
+          >
+            Close Modal
+          </button>
+        </Modal>
+      </React.Fragment>
+    );
+  }
+}
+export default HelpModal;
